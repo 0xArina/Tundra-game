c66bb6530c39c9e3f63dec123e294ea02cb2083f
Arina <43216874+rereshek@users.noreply.github.com>
2020-10-15 15:39:01 -0400

    updates

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -19,4 +19,9 @@ public class SceneLoader : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void BackToP1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
 }
