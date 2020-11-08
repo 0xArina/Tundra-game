beca538a1e682f2252fcf3c67e3559ba3564d7a8
Eric Moore <51104403+ericmoore123@users.noreply.github.com>
2020-11-07 22:53:09 -0500

    Added collider and enlarged image

---
+++ Assets/Scripts/onHoverShowZoom.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class onHoverShowZoom : MonoBehaviour
+{
+    public GameObject image;
+
+    public void Start()
+    {
+        image.SetActive(false);
+    }
+
+    public void OnMouseEnter()
+    {
+        image.SetActive(true);
+    }
+
+    public void OnMouseExit()
+    {
+        image.SetActive(false);
+    }
+}
