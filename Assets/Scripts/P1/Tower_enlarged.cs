5dc8c35e0b0f68eb6c7895543c39a1afc3541cde
Arina <43216874+rereshek@users.noreply.github.com>
2020-11-10 01:26:20 -0500

    figured out the focking enlargement

---
+++ Assets/Scripts/P1/Tower_enlarged.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tower_enlarged : MonoBehaviour
+{
+    public GameObject imageTower;
+
+    public void Start()
+    {
+        imageTower.SetActive(false);
+    }
+
+    public void OnMouseEnter()
+    {
+        imageTower.SetActive(true);
+        Debug.Log("Ray hit tower");
+    }
+
+    public void OnMouseDown()
+    {
+        SceneManager.LoadScene(8);
+    }
+
+    public void OnMouseExit()
+    {
+        imageTower.SetActive(false);
+    }
+}
