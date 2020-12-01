89d156ea747b86a743a407318f1a4df86f7704c2
Christina Merpaw <camerpaw@gmail.com>
2020-11-30 21:34:44 -0500

    Added music, trying to get music to stop on button click

---
+++ Assets/Scripts/MusicDestroyer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicDestroyer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+		if (objs.Length > 0)
+			Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/MusicKeeper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicKeeper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+		if (objs.Length > 1)
+			Destroy(this.gameObject);
+		
+		DontDestroyOnLoad(this.gameObject);
+    }
+}
