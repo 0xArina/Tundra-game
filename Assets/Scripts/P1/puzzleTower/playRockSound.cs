d2cb2ee806ed93dfe21990f74c1ed226336fe8cc
Christina Merpaw <camerpaw@gmail.com>
2020-12-10 07:31:08 -0500

    added sounds and music, fixed a couple of buttons

---
+++ Assets/Scripts/P1/puzzleTower/playRockSound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playRockSound : MonoBehaviour
+{
+	public AudioSource rockS;
+	public AudioClip[] rockClipArray;
+	
+	void Awake()
+	{
+		rockS = GetComponent<AudioSource> ();
+	}
+	
+    public void Rocky()
+    {
+        rockS.clip = rockClipArray[Random.Range(0, rockClipArray.Length)];
+		rockS.PlayOneShot (rockS.clip);
+    }
+}
